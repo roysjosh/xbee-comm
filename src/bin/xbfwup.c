f9ac2929fdd203ba1c6837c916085f9dc16178c1
Joshua Roys <roysjosh@gmail.com>
2011-02-06 17:56:33 -0500

    xbfwup: add xbee programing routines
    Add the XMODEM/CRC routines for firmware upload.  A few more outputs
    from the xbee should be checked, but programming is successful.
    Also, the bootloader was entered more reliably by sending \r every
    1/10 second over 2 seconds.

--- src/bin/xbfwup.c
+++ src/bin/xbfwup.c
@@ -1,11 +1,14 @@
 #define _BSD_SOURCE
 
+#include <endian.h>
 #include <err.h>
 #include <fcntl.h>
 #include <poll.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -23,6 +26,29 @@ wait_for_ok(int fd) {
 	} while (1);
 	read(fd, buf, 1);
 	read(fd, buf, 1);
+}
+
+ssize_t
+xb_read(int fd, char *buf, size_t count) {
+	ssize_t pos = 0, ret;
+	struct pollfd fds[1];
+
+	fds[0].fd = fd;
+	fds[0].events = POLLIN;
+
+	do {
+		ret = read(fd, buf + pos, count - pos);
+
+		if (ret <= 0) {
+			return ret;
+		}
+
+		pos += ret;
+
+		ret = poll(fds, 1, 100);
+	} while (ret > 0 && pos < count);
+
+	return pos;
 }
 
 ssize_t
@@ -46,7 +72,7 @@ xb_write(int fd, const char *buf, size_t count) {
 int
 xb_send_command(int fd, char *cmd, const char *format, ...) {
 	char buf[256];
-	size_t off = 0;
+	size_t off;
 	ssize_t ret;
 	va_list ap;
 
@@ -83,86 +109,249 @@ xb_send_command(int fd, char *cmd, const char *format, ...) {
 	return ret;
 }
 
+/* http://en.wikipedia.org/wiki/Computation_of_CRC */
+uint16_t
+xmodem_crc(const char *data) {
+	int i, j;
+	uint16_t rem = 0;
+
+	/* assume 128 byte blocks */
+	for(i = 0; i < 128; i++) {
+		rem ^= data[i] << 8;
+		for(j = 0; j < 8; j++) {
+			if (rem & 0x8000) {
+				rem = (rem << 1) ^ 0x1021;
+			}
+			else {
+				rem <<= 1;
+			}
+		}
+	}
+
+//	printf("%0hx\n", rem);
+
+	return rem;
+}
+
 int
-main() {
+xb_firmware_update(int xbfd, int fwfd) {
+	char *fwbuf;
+	char buf[128], header[3], reply[1];
+	size_t len, off;
+	ssize_t ret;
+	struct stat stbuf;
+	uint8_t block;
+	uint16_t crc;
+	unsigned int i;
+
+	/* at the menu: "1. upload ebl" */
+	if (xb_write(xbfd, "1", 1)) {
+		warnx("failed to enter programming mode");
+		return -1;
+	}
+
+	/* read reply: "\r\nbegin upload\r\nC" */
+	if ( (ret = xb_read(xbfd, buf, sizeof(buf))) <= 0) {
+		warnx("failed to read programming go-ahead");
+		return -1;
+	}
+	if (buf[ret - 1] != 'C') {
+		warnx("unknown transfer type");
+		return -1;
+	}
+
+	/* read entire firmware image into memory */
+	if (fstat(fwfd, &stbuf) < 0) {
+		warn("failed to stat firmware file");
+		return -1;
+	}
+	if (stbuf.st_size == 0) {
+		warnx("empty firmware file!");
+		return -1;
+	}
+
+	/* round up to nearest 128 byte block */
+	len = stbuf.st_size;
+	len += (len % 128 ? 128 - (len % 128) : 0);
+
+	if ( (fwbuf = malloc(len)) == NULL) {
+		warn("failed to allocate memory");
+		return -1;
+	}
+
+	/* set "empty" bytes to 0xff */
+	memset(fwbuf + stbuf.st_size, 0xff, len - stbuf.st_size);
+
+	off = 0;
+	do {
+		ret = read(fwfd, fwbuf + off, len - off);
+
+		if (ret < 0) {
+			return -1;
+		}
+
+		off += ret;
+	} while (ret);
+
+	printf("Read %i byte firmware file (%i blocks).\n",
+			(int)stbuf.st_size, (int)len / 128);
+
+	/* send it */
+	for(block = 1, i = 0; i < len / 128; block++, i++) {
+		header[0] = (uint8_t)'\x01'; /* SOH */
+		header[1] = (uint8_t)block;
+		header[2] = (uint8_t)(255 - block);
+
+		if (xb_write(xbfd, header, 3)) {
+			warn("failed to write XMODEM header, block %i", i);
+			return -1;
+		}
+
+		if (xb_write(xbfd, fwbuf + (i * 128), 128)) {
+			warn("failed to write XMODEM data, block %i", i);
+			return -1;
+		}
+
+		/* CRC-16 */
+		crc = xmodem_crc(fwbuf + (i * 128));
+		crc = htobe16(crc);
+		if (xb_write(xbfd, (const char *)&crc, 2)) {
+			warn("failed to write XMODEM CRC, block %i", i);
+			return -1;
+		}
+
+		/* read ACK (0x06); use read for speed! */
+		if (read(xbfd, reply, 1) <= 0 || *reply != '\x06') {
+			warnx("failed to transfer block %i: %02x", i, *reply);
+			return -1;
+		}
+
+		/* display progress */
+		printf(".");
+		if ((i + 1) % 50 == 0) {
+			printf(" %4i\n", (i + 1));
+		}
+		fflush(stdout);
+	}
+
+	printf("\n");
+
+	/* write EOT (0x04) */
+	if (xb_write(xbfd, "\x04", 1)) {
+		warn("failed to write XMODEM EOT");
+		return -1;
+	}
+
+	/* read reply: "\x06\r\nSerial upload complete\r\n" */
+	if (xb_read(xbfd, buf, sizeof(buf)) <= 0 || *buf != '\x06') {
+		warnx("failed to read programming confirmation");
+		return -1;
+	}
+
+	return 0;
+}
+
+int
+main(int argc, char *argv[]) {
 	char buf[1024];
-	int fd, i, ret;
-	ssize_t pos, sret;
-	struct pollfd fds[1];
+	int fwfd, xbfd, i;
+	ssize_t ret;
 	struct termios serial;
 
-	if ( (fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY)) < 0) {
+	if (argc < 2) {
+		errx(EXIT_FAILURE, "missing parameter: %s <file.ebl>", argv[0]);
+	}
+
+	if ( (fwfd = open(argv[1], O_RDONLY)) < 0) {
+		err(EXIT_FAILURE, "failed to open firmware file: %s", argv[1]);
+	}
+
+	if ( (xbfd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY)) < 0) {
 		err(EXIT_FAILURE, "failed to open serial console");
 	}
 
-	if (tcgetattr(fd, &serial)) {
+	if (tcgetattr(xbfd, &serial)) {
 		err(EXIT_FAILURE, "failed to get terminal attributes");
 	}
 
+//	if (!recovery_mode)
+
 	/* enter command mode */
+	printf("Entering AT command mode...\n");
 	sleep(1);
-	write(fd, "+++", 3);
+	write(xbfd, "+++", 3);
 	sleep(1);
-	wait_for_ok(fd);
+	wait_for_ok(xbfd);
+
+	printf("Entering bootloader...\n");
 
 	/* start the power cycle */
-	xb_send_command(fd, "FR", "");
+	xb_send_command(xbfd, "FR", "");
 
 	/* assert DTR, clear RTS */
 	i = TIOCM_DTR | TIOCM_CTS;
-	ioctl(fd, TIOCMSET, &i);
+	ioctl(xbfd, TIOCMSET, &i);
 
 	/* send a serial break */
-	ioctl(fd, TIOCSBRK);
+	ioctl(xbfd, TIOCSBRK);
 
 	/* wait for the power cycle to hit */
 	sleep(2);
 
 	/* clear the serial break */
-	ioctl(fd, TIOCCBRK);
+	ioctl(xbfd, TIOCCBRK);
 
 	/* RTS/CTS have an annoying habit of toggling... */
 	i = TIOCM_DTR | TIOCM_CTS;
-	ioctl(fd, TIOCMSET, &i);
+	ioctl(xbfd, TIOCMSET, &i);
 
 	/* send a carriage return at 115200bps */
 	cfsetspeed(&serial, B115200);
-	if (tcsetattr(fd, TCSANOW, &serial)) {
-		err(EXIT_FAILURE, "failed to set 115200bps");
+	/* don't wait more than 1/10s for input */
+	serial.c_cc[VMIN] = 0;
+	serial.c_cc[VTIME] = 1;
+	if (tcsetattr(xbfd, TCSANOW, &serial)) {
+		err(EXIT_FAILURE, "failed to set 115200bps, VMIN/VTIME");
 	}
 
-	for(i = 0; i < 10; i++) {
-		write(fd, "\r", 1);
-		sret = read(fd, buf, sizeof(buf));
-		if (sret > 0)
+	for(i = 0; i < 20; i++) {
+		xb_write(xbfd, "\r", 1);
+		if ( (ret = xb_read(xbfd, buf, sizeof(buf))) > 0) {
 			break;
-		usleep(100000);
-	}
-	if (sret == 0) {
-		errx(EXIT_FAILURE, "timeout waiting for bootloader prompt");
+		}
 	}
 
-	fds[0].fd = fd;
-	fds[0].events = POLLIN;
+	/* check for "BL >" prompt */
 
-	pos = 0;
-	do {
-		sret = read(fd, buf + pos, sizeof(buf) - pos);
-		if (sret > 0)
-			pos += sret;
-		ret = poll(fds, 1, 100);
-	} while (ret > 0 && pos < sizeof(buf));
+	/* restore "wait forever" settings */
+	serial.c_cc[VMIN] = 1;
+	serial.c_cc[VTIME] = 0;
+	if (tcsetattr(xbfd, TCSANOW, &serial)) {
+		err(EXIT_FAILURE, "failed to reset VMIN/VTIME");
+	}
+
+	printf("Beginning programming...\n");
+
+	/* update! */
+	if (xb_firmware_update(xbfd, fwfd)) {
+		errx(EXIT_FAILURE, "failed to flash firmware!");
+	}
+
+	/* verify */
+
+	printf("Programming complete, running uploaded firmware...\n");
 
 	/* run the firmware */
-	write(fd, "2", 1);
+	xb_write(xbfd, "2", 1);
 
 	/* cleanup */
 	cfsetspeed(&serial, B9600);
-	if (tcsetattr(fd, TCSANOW, &serial)) {
+	if (tcsetattr(xbfd, TCSANOW, &serial)) {
 		err(EXIT_FAILURE, "failed to set 9600bps");
 	}
 
-	close(fd);
+	close(fwfd);
+	close(xbfd);
 
 	return EXIT_SUCCESS;
 }
